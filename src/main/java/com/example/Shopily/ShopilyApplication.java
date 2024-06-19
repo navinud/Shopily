21e4693993c72b24a234e91e6b406ef84052b60e
navinud <ilangakoonipnd.21@uom.lk>
2024-06-19 09:02:56 +0530

    initial 1

---
+++ src/main/java/com/example/Shopily/ShopilyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Shopily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Shopily/ShopilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shopily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
